7dc458700e359ea89f499f735972e61004cd9e2e
Michael Dziedzic <mdz@eggfoo.com>
2009-06-30 16:19:33 +0000

    Working on faq.php.
    git-svn-id: file:///Users/michael/repos-aroundtheworld/trunk@13 f54931ca-e29a-4586-8446-2c8b6a4cd0ca

--- wp-content/themes/480days/faq.php
+++ wp-content/themes/480days/faq.php
@@ -20,9 +20,76 @@ Template Name: FAQ
 
 					<!-- ================================================= CONTENT -->	
 
-					<div id="content"><div id="content-about">
+					<div id="content"><div id="content-faq">
 						<img src="<?php bloginfo('template_url'); ?>/images/content/faq/title.png" width="380" height="90" alt="FAQ" />
 						<div id="cont-container">
+						
+						
+							<div class="faq-section">
+								<h2>Planning</h2>
+								<?php query_posts("cat=182&order=ASC") ?>
+								<?php if (have_posts()) : ?>
+									<?php $counter = 0; ?>
+									<?php while (have_posts()) : the_post(); ?>
+										<div class="faq-question">
+											<div class="comments-counter">
+												<p><a href="<?php the_permalink() ?>#comments-link"><img src="<?php bloginfo('template_url'); ?>/images/content/bubble.png" width="11" height="10" alt="Comments" /> <?php comments_number('0', '1', '%' );?></a></p>
+											</div>
+											<div class="faq-x">
+												<img src="<?php bloginfo('template_url'); ?>/images/content/x.png" width="7" height="7" alt="x" />
+											</div>
+											<p class="entry-link"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>			
+										</div>
+									<?php endwhile; ?>								
+								<?php else : ?>
+									Not found (no posts)
+								<?php endif; ?>																		
+							</div>
+
+							<div class="faq-section">
+								<h2>Life on the Road</h2>
+								<?php query_posts("cat=188&order=ASC") ?>
+								<?php if (have_posts()) : ?>
+									<?php $counter = 0; ?>
+									<?php while (have_posts()) : the_post(); ?>
+										<div class="faq-question">
+											<div class="comments-counter">
+												<p><a href="<?php the_permalink() ?>#comments-link"><img src="<?php bloginfo('template_url'); ?>/images/content/bubble.png" width="11" height="10" alt="Comments" /> <?php comments_number('0', '1', '%' );?></a></p>
+											</div>
+											<div class="faq-x">
+												<img src="<?php bloginfo('template_url'); ?>/images/content/x.png" width="7" height="7" alt="x" />
+											</div>
+											<p class="entry-link"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>			
+										</div>
+									<?php endwhile; ?>								
+								<?php else : ?>
+									Not found (no posts)
+								<?php endif; ?>																		
+							</div>
+
+							<div class="faq-section">
+								<h2>Most and Least Favorites</h2>
+								<?php query_posts("cat=16&order=ASC") ?>
+								<?php if (have_posts()) : ?>
+									<?php $counter = 0; ?>
+									<?php while (have_posts()) : the_post(); ?>
+										<div class="faq-question">
+											<div class="comments-counter">
+												<p><a href="<?php the_permalink() ?>#comments-link"><img src="<?php bloginfo('template_url'); ?>/images/content/bubble.png" width="11" height="10" alt="Comments" /> <?php comments_number('0', '1', '%' );?></a></p>
+											</div>
+											<div class="faq-x">
+												<img src="<?php bloginfo('template_url'); ?>/images/content/x.png" width="7" height="7" alt="x" />
+											</div>
+											<p class="entry-link"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>			
+										</div>
+									<?php endwhile; ?>								
+								<?php else : ?>
+									Not found (no posts)
+								<?php endif; ?>																		
+							</div>
+
+
+						
 						</div>
 					</div></div>
 
