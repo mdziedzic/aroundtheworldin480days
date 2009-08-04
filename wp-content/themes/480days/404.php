4b1a51b8bfaf39a6835c83ee191120ad2073b9ce
Michael Dziedzic <mdz@eggfoo.com>
2009-08-04 23:01:29 +0000

    COMPLETE: 404 File Not Found (404.php).
    git-svn-id: file:///Users/michael/repos-aroundtheworld/trunk@85 f54931ca-e29a-4586-8446-2c8b6a4cd0ca

--- wp-content/themes/480days/404.php
+++ wp-content/themes/480days/404.php
@@ -16,21 +16,11 @@
 					<div id="content"><div id="content-about">
 						<img src="<?php bloginfo('template_url'); ?>/images/content/notfound/title.png" width="440" height="90" alt="404 Page Not Found" />
 						<div id="emily-michael-photo">
-							<img src="<?php bloginfo('template_url'); ?>/images/content/about/us.jpg" width="442" height="284" class="photo-horiz" alt="Emily and Michael" />
-							<p class="photo-tagline-horiz">Emily and Michael in Xi'an, China.</p>
+							<img src="<?php bloginfo('template_url'); ?>/images/content/notfound/notfound.jpg" width="442" height="284" class="photo-horiz" alt="Michael reading the New Testament" />
+							<p class="photo-tagline-horiz">"I was lost but now... still lost."</p>
 						</div>
 						
 						<div id="cont-container">
-							<?php include (TEMPLATEPATH . '/font-size.php'); ?>
-							<p class="body-text-first">Lorem ipsum dolor sit amet, maecenas leo eos integer voluptatibus et molestie, aliquet purus pellentesque amet vestibulum amet risus, lobortis eu venenatis blandit velit id adipiscing, adipiscing sed lorem non phasellus quis. Fringilla massa sed, justo eget sociis aliquam consequat justo hic, parturient sed a nec accumsan mattis suspendisse, nunc vel pulvinar, sit urna et in. Risus vestibulum mattis, at per. Dignissim convallis vel, sed sapien luctus conubia nibh, adipiscing hendrerit, vitae lacinia pede faucibus. Lectus aliquam beatae, mauris felis vitae proin diam, sed aenean, tempor leo rhoncus. Faucibus eros arcu elementum quis pellentesque sed. Magna orci vulputate eget, mauris inceptos libero rutrum ut in feugiat, lacinia et. Sociosqu tortor nibh et, consectetuer duis ornare dolor at consequat, aenean molestie non elit urna nibh mi, risus commodo, penatibus nec arcu dolor suspendisse augue dui. Ac quisque feugiat vivamus nulla, suscipit tellus orci nisl. Ac adipiscing consequat erat eget laoreet justo, lacus cras aenean interdum ultricies, sed id nunc ut sollicitudin nullam, erat libero.</p>
-							<p class="body-text">Consequat libero praesent euismod congue dictum tincidunt, wisi eu wisi. Felis in, lorem tincidunt. Ligula ut, faucibus metus qui. Sed parturient non ante mauris, vel mattis sit pharetra in nec eu, nec pede aenean quia pellentesque. Maecenas morbi sit vestibulum wisi habitasse. Felis platea in est quam erat.</p>
-							<p class="body-text">Orci nibh sed eget accumsan aliquam, sed tempor risus fringilla, fringilla vitae pede, platea aenean posuere ridiculus ullamcorper ut dignissim, libero semper vel nibh quis. Imperdiet in adipiscing amet dui rhoncus ante. Mattis lacus ipsum in, dolor condimentum sed, sit eros lobortis aenean convallis quis, vestibulum et orci sed, lorem dapibus. Vivamus pharetra. Cursus dolor porta id pede neque, neque imperdiet nunc placerat sed erat. Tempor justo bibendum magna nisl dapibus interdum, pellentesque mauris, mollis faucibus mattis ut justo, dictum integer. Ornare lacus faucibus urna wisi lectus, montes potenti augue. Imperdiet diam, augue sit ac nunc nec accumsan quo.</p>
-							<p class="body-text">Orci nibh sed eget accumsan aliquam, sed tempor risus fringilla, fringilla vitae pede, platea aenean posuere ridiculus ullamcorper ut dignissim, libero semper vel nibh quis. Imperdiet in adipiscing amet dui rhoncus ante. Mattis lacus ipsum in, dolor condimentum sed, sit eros lobortis aenean convallis quis, vestibulum et orci sed, lorem dapibus. Vivamus pharetra. Cursus dolor porta id pede neque, neque imperdiet nunc placerat sed erat. Tempor justo bibendum magna nisl dapibus interdum, pellentesque mauris, mollis faucibus mattis ut justo, dictum integer. Ornare lacus faucibus urna wisi lectus, montes potenti augue. Imperdiet diam, augue sit ac nunc nec accumsan quo.</p>
-							<div class="text-break">
-								<img src="<?php bloginfo('template_url'); ?>/images/content/text-break.png" width="78" height="18" alt="Text break" />
-							</div>
-							
-						
 						</div>
 					</div></div>
 
