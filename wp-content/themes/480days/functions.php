cf0544c403198f17a1e90ce31b757abe1f2cd139
Michael Dziedzic <mdz@eggfoo.com>
2009-08-19 17:11:21 +0000

    Working on adding location info to category and single pages.
    Made sure location doesn't appear on FAQ pages.
    
    git-svn-id: file:///Users/michael/repos-aroundtheworld/trunk@95 f54931ca-e29a-4586-8446-2c8b6a4cd0ca

--- wp-content/themes/480days/functions.php
+++ wp-content/themes/480days/functions.php
@@ -1,5 +1,18 @@
 <?php
 
+function post_is_in_descendant_category( $cats, $_post = null )
+{
+	foreach ( (array) $cats as $cat ) {
+		// get_term_children() accepts integer ID only
+		$descendants = get_term_children( (int) $cat, 'category');
+		if ( $descendants && in_category( $descendants, $_post ) )
+			return true;
+	}
+	return false;
+}
+
+
+
 function mytheme_comment($comment, $args, $depth) {
    $GLOBALS['comment'] = $comment; 
    global $cmntCnt;
--- wp-content/themes/480days/location.php
+++ wp-content/themes/480days/location.php
@@ -1,13 +1,19 @@
-	<div class="location">
-	<?php
-		$category = get_the_category($post->ID); 
-		$catName = substr($category[0]->cat_name, 4);
+<?php 
+	if (! post_is_in_descendant_category( 192 )) {
+?>
+
+		<div class="location">
+		<?php	
+			$category = get_the_category($post->ID); 
+			$catName = substr($category[0]->cat_name, 4);
+			
+			$categoryParent = get_category_parents($category[0], FALSE, '/', FALSE);
+			$categoryParent = substr($categoryParent, 4);
+			$chopPoint = strcspn($categoryParent, '/');
+			$categoryParent = substr($categoryParent, 0, $chopPoint);
 		
-		$categoryParent = get_category_parents($category[0], FALSE, '/', FALSE);
-		$categoryParent = substr($categoryParent, 4);
-		$chopPoint = strcspn($categoryParent, '/');
-		$categoryParent = substr($categoryParent, 0, $chopPoint);
-	
-	?>
-		<p><?php echo $catName . ', ' . $categoryParent; ?></p>
-	</div>
+		?>
+			<p><?php echo $catName . ', ' . $categoryParent; ?></p>
+		</div>
+		
+<?php } ?>
