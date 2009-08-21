b10d718721437930e4cbf0faa490dbcacdcb6f07
Michael Dziedzic <mdz@eggfoo.com>
2009-08-21 19:52:47 +0000

    Working on making location link to category page on single post pages only.
    git-svn-id: file:///Users/michael/repos-aroundtheworld/trunk@108 f54931ca-e29a-4586-8446-2c8b6a4cd0ca

--- wp-content/themes/480days/location.php
+++ wp-content/themes/480days/location.php
@@ -1,6 +1,7 @@
 		<div class="location">
 		<?php	
 			$category = get_the_category($post->ID); 
+			$category_link = get_category_link($category[0]->cat_ID);
 			$catName = substr($category[0]->cat_name, 4);
 			
 			$categoryParent = get_category_parents($category[0], FALSE, '/', FALSE);
@@ -9,5 +10,14 @@
 			$categoryParent = substr($categoryParent, 0, $chopPoint);
 		
 		?>
-			<p><?php echo $catName . ', ' . $categoryParent; ?></p>
+		
+		<?php
+			if (is_single()) { ?>
+				<p><a href="<?php echo $category_link ?>"><?php echo $catName . ', ' . $categoryParent; ?></a></p>
+		<?php
+			} else { ?>	
+				<p><?php echo $catName . ', ' . $categoryParent; ?></p>	
+		<?php
+			} ?>
+			
 		</div>
