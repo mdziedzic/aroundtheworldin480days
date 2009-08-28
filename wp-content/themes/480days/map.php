090f4362cf863a97dabb94f09c0cef6f8fd24ff4
Michael Dziedzic <mdz@eggfoo.com>
2009-08-28 00:13:24 +0000

    COMPLETED: Working on trying to get map to load only after actually hitting "MAP" button in navigation.
    git-svn-id: file:///Users/michael/repos-aroundtheworld/trunk@118 f54931ca-e29a-4586-8446-2c8b6a4cd0ca

--- wp-content/themes/480days/map.php
+++ wp-content/themes/480days/map.php
@@ -11,9 +11,9 @@ Template Name: Map
     							'&marker_select_center=true&marker_select_highlight=false' . 
     							'&marker_select_info_window=true&marker_select_attachments=true&auto_info_open=true');
 	} else {
-		echo GeoMashup::map('map_content=global&' . 
-    							'&marker_select_center=false&marker_select_highlight=false' . 
-    							'&marker_select_info_window=false&marker_select_attachments=true&auto_info_open=false');
+		echo GeoMashup::map('map_content=global&open_object_id=848' . '&zoom=2' . '&center_lat=30&center_lng=133' .
+    							'&marker_select_center=true&marker_select_highlight=false' . 
+    							'&marker_select_info_window=true&marker_select_attachments=true&auto_info_open=false');
 	}
 
 ?>
