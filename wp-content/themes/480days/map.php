c988e7d86dce9a7c89ad517dc807d097a46eed5c
Michael Dziedzic <mdz@eggfoo.com>
2011-10-11 23:56:36 +0000

    GeoMashup now pretty much works correctly.
    Still need to test on Windows.
    Still need to think about adding keyboard control.
    
    git-svn-id: file:///Users/michael/repos-aroundtheworld/trunk@256 f54931ca-e29a-4586-8446-2c8b6a4cd0ca

--- wp-content/themes/480days/map.php
+++ wp-content/themes/480days/map.php
@@ -5,14 +5,15 @@ Template Name: Map
 ?>
 
 <?php
-
-	if ($_GET['mapLocation'] != "5000") {
-		echo GeoMashup::map('map_content=global&open_object_id=' . $_GET['mapLocation'] . 
-    							'&marker_select_center=true&marker_select_highlight=false' . 
+	$mapLoc = $_GET['mapLocation'];
+	
+	if ($mapLoc != "5000") {
+		echo GeoMashup::map('map_content=global&open_object_id=' . $mapLoc . '&object_id=' . $mapLoc .
+    							'&marker_select_center=false&marker_select_highlight=false' . 
     							'&marker_select_info_window=true&marker_select_attachments=true&auto_info_open=true');
 	} else {
 		echo GeoMashup::map('map_content=global&open_object_id=848' . '&zoom=2' . '&center_lat=30&center_lng=133' .
-    							'&marker_select_center=true&marker_select_highlight=false' . 
+    							'&marker_select_center=false&marker_select_highlight=false' . 
     							'&marker_select_info_window=true&marker_select_attachments=true&auto_info_open=false');
 	}
 
