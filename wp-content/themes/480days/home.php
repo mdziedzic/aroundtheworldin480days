68d0e48b667f8e44018f992719a8bd5750ab63df
Michael Dziedzic <mdz@eggfoo.com>
2013-08-28 17:24:17 +0000

    Edited text on home page.
    git-svn-id: file:///Users/michael/repos-aroundtheworld/trunk@357 f54931ca-e29a-4586-8446-2c8b6a4cd0ca

--- wp-content/themes/480days/home.php
+++ wp-content/themes/480days/home.php
@@ -17,16 +17,16 @@
 						<img id="home-image" src="<?php bloginfo('template_url'); ?>/images/content/home/aroundtheworldin480days.png" width="540" height="342" alt="Around the World in 480 Days" />
 
 						<div id="cont-container">
+						
 							<h1>Australia to South Africa, entirely by land.*</h1>
-							<h2>A 34-country odyssey involving unreasonable monkeys, malaria, rashes, leeches, scuffles with locals, stand off with military police, great joy, deep despair, and probably a bit too much of the local beer.</h2>
+							<h2>A 480-day, 34-country odyssey featuring an over-caffeinated monkey, leeches, rashes, cerebral malaria, scuffles with the indigenous population, a standoff or two with military police, great joy, deep despair, lots of fatigue, and probably a bit too much of the local beer (wine, mead, whiskey, tej, waragi…).</h2>
 							<div class="text-break">
 								<img src="<?php bloginfo('template_url'); ?>/images/content/text-break.png" width="78" height="18" alt="Text break" />
 							</div>
 							<p class="body-text-first-home">*And a few bits of river and sea, but really, mostly land.</p>
-							<p class="body-text-home">And, well, to be perfectly honest, really more Singapore to Cape Town, South Africa by land as a flight from Perth, Australia to Bali, Indonesia because some Aussie holiday messed up our plans of taking a ferry from Darwin.</p> 
-							<p class="body-text-home">And, while we are at it, though we did spend a good amount of effort trying, the land borders between Thailand and Myanmar were closed, even to considerable efforts and contemplated bribery, so we had to take a small round-trip flight between Chiang Mai, Thailand and Yangon.</p>
-							<p class="body-text-home">And in the interest of full disclosure, a small prop plane in Ethiopia from Lalibella to Addis Ababa, mainly to save time as we did go from Addis Abab to Lalibella by land.</p>
-							<p class="body-text-home">And really, it was 480 days plus or minus a few as it took us awhile to make it back home once we got back to the U.S., had to catch up with friends and family.</p>
+							<p class="body-text-home">And, well, to be perfectly honest, really more Singapore to Cape Town by land as a flight from Perth to Bali was needed because some Aussie holiday messed up our plans of taking a ferry or something from Darwin.</p>
+							<p class="body-text-home">And, while we are at it, though we did spend a good amount of effort trying, the land borders between Thailand and Myanmar were closed, and despite considerable efforts and contemplated bribery, we had to take a small round-trip flight between Chiang Mai, Thailand and Yangon to get in and out of Myanmar.</p>
+							<p class="body-text-home">And in the interest of full disclosure, a small prop plane in Ethiopia from Lalibela to Addis Ababa as we had already done the trip in reverse by bus (it was near the end our trip and we were running out of steam).</p>
 						</div>
 						<div id="previous-next">
 								<div id="next">
