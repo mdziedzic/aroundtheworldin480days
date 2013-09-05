433219b2fdf16c10b3148eeae74fdfc9029da44c
Michael Dziedzic <mdz@eggfoo.com>
2013-09-05 17:21:16 +0000

    Changed some text.
    git-svn-id: file:///Users/michael/repos-aroundtheworld/trunk@371 f54931ca-e29a-4586-8446-2c8b6a4cd0ca

--- wp-content/themes/480days/404.php
+++ wp-content/themes/480days/404.php
@@ -17,7 +17,7 @@
 						<img src="<?php bloginfo('template_url'); ?>/images/content/notfound/title.png" width="440" height="90" alt="404 Page Not Found" />
 						<div id="emily-michael-photo">
 							<img src="<?php bloginfo('template_url'); ?>/images/content/notfound/notfound.jpg" width="442" height="284" class="photo-horiz" alt="Michael reading the New Testament" />
-							<p class="photo-tagline-horiz">"I was lost but now... still lost."</p>
+							<p class="photo-tagline-horiz">"I once was lost but now... still lost."</p>
 						</div>
 						
 						<div id="cont-container">
