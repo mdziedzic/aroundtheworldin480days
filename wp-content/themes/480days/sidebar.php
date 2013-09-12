15fc57026c75eb3a16b2dbaff44052d7ea45f51e
Michael Dziedzic <mdz@eggfoo.com>
2013-09-12 06:14:19 +0000

    Rearranged Photo Series sets in the sidebar.
    git-svn-id: file:///Users/michael/repos-aroundtheworld/trunk@423 f54931ca-e29a-4586-8446-2c8b6a4cd0ca

--- wp-content/themes/480days/sidebar.php
+++ wp-content/themes/480days/sidebar.php
@@ -33,14 +33,14 @@
 										
 										<br />
 										<ul>
-											<li><a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157624185440774/" title="Our Favorites">Favorites</a></li>
-											<li><a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157624185407928/" title="Food">Food</a></li>
-											<li><a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157624185410090/" title="People">People</a></li>
 											<li><a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157624185415868/" title="Architecture">Architecture</a></li>
-											<li><a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157624185420762/" title="Markets">Markets</a></li>
+											<li><a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157635484723021/" title="Detail">Detail</a></li>	
+											<li><a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157624185440774/" title="Favorites">Favorites</a></li>
+											<li><a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157624185407928/" title="Food">Food</a></li>
+											<li><a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157635492199404/" title="Markets">Markets</a></li>
+											<li><a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157624185410090/" title="People">People</a></li>
 											<li><a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157624061071723/" title="Transportation">Transportation</a></li>
-											<li><a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157624185431392/" title="Typography">Typography</a></li>	
-											<li><a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157624185436968/" title="Detail">Detail</a></li>	
+											<li><a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157635484865407/" title="Typography">Typography</a></li>	
 											<li><a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157624443442383/" title="Us">Us</a></li>
 										</ul>
 
