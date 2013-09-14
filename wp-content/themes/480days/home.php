e96712e52e63695c7868796bf5891d6bf1acc95e
Michael Dziedzic <mdz@eggfoo.com>
2013-09-14 19:09:56 +0000

    Enlarged asterix on homepage.
    git-svn-id: file:///Users/michael/repos-aroundtheworld/trunk@433 f54931ca-e29a-4586-8446-2c8b6a4cd0ca

--- wp-content/themes/480days/home.php
+++ wp-content/themes/480days/home.php
@@ -17,7 +17,7 @@
 							<div class="text-break">
 								<img src="<?php bloginfo('template_url'); ?>/images/content/text-break.png" width="78" height="18" alt="Text break" />
 							</div>
-							<p class="body-text-first-home">*And a few bits of river and sea, but really, mostly land.</p>
+							<p class="body-text-first-home"><span id="big-asterix">*</span>And a few bits of river and sea, but really, mostly land.</p>
 							<p class="body-text-home">And, well, to be perfectly honest, really more Singapore to Cape Town by land as a flight from Perth to Bali was needed because Easter messed up our plans of taking a ferry or something from Darwin.</p>
 							<p class="body-text-home">And, while we are at it, though we did spend a good amount of effort trying, the land borders between Thailand and Myanmar were closed, and despite considerable efforts and contemplated bribery, we had to take a small round-trip flight between Chiang Mai, Thailand and Yangon to get in and out of Myanmar.</p>
 							<p class="body-text-home">And in the interest of full disclosure, a small prop plane in Ethiopia from Lalibela to Addis Ababa as we had already done the trip in reverse by bus (it was near the end our trip and we were running out of steam).</p>
