d2e3cea3e95d5cb787826cb8615286eecfbfb6b0
Michael Dziedzic <mdz@eggfoo.com>
2014-10-22 03:08:33 +0000

    No HTML tags allowed in comments.
    git-svn-id: file:///Users/michael/repos-aroundtheworld/trunk@492 f54931ca-e29a-4586-8446-2c8b6a4cd0ca

--- wp-content/themes/480days/functions.php
+++ wp-content/themes/480days/functions.php
@@ -1,5 +1,10 @@
 <?php
 
+
+add_filter('comment_text', 'wp_filter_nohtml_kses');
+add_filter('comment_text_rss', 'wp_filter_nohtml_kses');
+add_filter('comment_excerpt', 'wp_filter_nohtml_kses');
+
 if ( function_exists('register_sidebar') )
     register_sidebar();
     
