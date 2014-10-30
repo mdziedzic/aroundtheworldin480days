330885ba3c3aa0c14072106e99cc66ea53ba0d90
Michael Dziedzic <mdz@eggfoo.com>
2014-10-30 21:34:27 +0000

    Retina graphics.
    Also some homepage and about text edits.
    
    git-svn-id: file:///Users/michael/repos-aroundtheworld/trunk@504 f54931ca-e29a-4586-8446-2c8b6a4cd0ca

--- wp-content/themes/480days/home.php
+++ wp-content/themes/480days/home.php
@@ -12,18 +12,20 @@
 
 						<div id="cont-container">
 						
-							<h1>Tasmania to Cape Town by Land<sup class="sup-h1">&dagger;</sup></h1>
+							<h1>Tasmania to Cape Town by Land<sup>*</sup></h1>
 
-							<h2>A 34-country, 480-day, 30,000-mile gallivanting gallimaufry featuring dubious transport through untold landscapes, a standoff or two (or three) with the locals, a drunk role-player, a circus bear, and at least one dangerously caffeinated monkey. And probably a bit too much of the regional beer.<sup>&Dagger;</sup> Oh, and lots of mosquitos. Along the way we took some <a href="https://www.flickr.com/photos/aroundtheworldin480days/sets/" title="Photos">photos</a> (here are some of our <a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157624185440774/" title="Favorite photos">favorites</a>).</h2>
+							<h2>34 countries, 480 days, 30,000 miles. Transport dubious, landscapes untold. A standoff or two<sup>&dagger;</sup> with the locals. A drunken role-player and a dwarven army. A circus bear and a caffeinated monkey. And probably a bit too much of the homegrown beer.<sup>&Dagger;</sup> Oh, and lots of mosquitoes. Along the way we took some <a href="https://www.flickr.com/photos/aroundtheworldin480days/sets/" title="Photos">photos</a> (here are some of our <a href="http://www.flickr.com/photos/aroundtheworldin480days/sets/72157624185440774/" title="Favorite photos">favorites</a>).</h2>
 							<div class="text-break">
 								<img src="<?php bloginfo('template_url'); ?>/images/content/text-break.png" width="78" height="18" alt="Text break" />
 							</div>
-							<p class="body-text-first-home"><sup>&dagger;</sup>And a few bits of river and sea, but really, almost entirely by land.</p>
+							<p class="body-text-first-home"><sup>*</sup>And a few bits of river and sea, but really, almost entirely by land.</p>
 							<p class="body-text-home">And, well, to be perfectly honest, really more Indonesia to South Africa by surface as a flight from Perth to Bali was needed to get up and out of the Land Down Under.</p>
 							<p class="body-text-home">And, while we are at it, though we did spend a fair amount of effort trying, the land border between Thailand and Myanmar was closed, and despite considerable effort and contemplated bribery, we had to take a short round-trip flight between Chiang Mai, Thailand and Yangon to get in and out of Myanmar.</p>
 							<p class="body-text-home">And in the interest of full disclosure, a small prop plane in Ethiopia from Lalibela to Addis Ababa as we had already done the trip in reverse by bus, it was near the end our trip, and we were running out steam (money, and, quite frankly, the will to live).</p>
 							<br/>
-							<p class="body-text-first-home"><sup>&Dagger;</sup>Wine, whisky, vodka, mead, waragi, arak, betel, kef, tej...</p>
+							<p class="body-text-first-home"><sup>&dagger;</sup>Or three...</p>
+							<br/>
+							<p class="body-text-first-home"><sup>&Dagger;</sup>Wine, whiskey, vodka, mead, waragi, arak, betel, kef, tej...</p>
 
 
 							<?php include (TEMPLATEPATH . '/share.php'); ?>
