58b3d7b7efb09432900791c5ead036f88e6467eb
Michael Dziedzic <mdz@eggfoo.com>
2014-11-05 17:11:57 +0000

    Changed initial map location.
    git-svn-id: file:///Users/michael/repos-aroundtheworld/trunk@516 f54931ca-e29a-4586-8446-2c8b6a4cd0ca

--- wp-content/themes/480days/map.php
+++ wp-content/themes/480days/map.php
@@ -12,7 +12,7 @@ Template Name: Map
     							'&marker_select_center=false&marker_select_highlight=false' . 
     							'&marker_select_info_window=true&marker_select_attachments=true&auto_info_open=true');
 	} else {
-		echo GeoMashup::map('map_content=global&open_object_id=848' . '&zoom=2' . '&center_lat=30&center_lng=133' .
+		echo GeoMashup::map('map_content=global&open_object_id=848' . '&zoom=2' . '&center_lat=25&center_lng=105' .
     							'&marker_select_center=false&marker_select_highlight=false' . 
     							'&marker_select_info_window=true&marker_select_attachments=true&auto_info_open=false');
 	}
