e4bf4511d4ae58f146f7b862d1160cf6ed2f05df
Michael Dziedzic <mdz@eggfoo.com>
2014-11-10 21:11:11 +0000

    Retina graphics.
    git-svn-id: file:///Users/michael/repos-aroundtheworld/trunk@523 f54931ca-e29a-4586-8446-2c8b6a4cd0ca

--- wp-content/themes/480days/share.php
+++ wp-content/themes/480days/share.php
@@ -16,24 +16,24 @@
       									'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href), 
       									'facebook-share-dialog', 
       									'width=626,height=436'); 
-    									return false;"><img src="<?php bloginfo('template_url'); ?>/images/content/share/facebook.png" width="16" height="16" alt="Facebook Share" /></a></li>
+    									return false;"><img src="<?php bloginfo('template_url'); ?>/images/content/share/facebook2x.png" width="16" height="16" alt="Facebook Share" /></a></li>
 
 								<li><a href="#" onclick="window.open(
       									'https://twitter.com/share?url=' +  <?php echo "'" . site_url() . "'"; ?>, 
       									'twitter-share-dialog',
       									'width=626,height=436'); 
-    									return false;"><img src="<?php bloginfo('template_url'); ?>/images/content/share/twitter.png" width="16" height="16" alt="Twitter Share" /></a></li>
+    									return false;"><img src="<?php bloginfo('template_url'); ?>/images/content/share/twitter2x.png" width="16" height="16" alt="Twitter Share" /></a></li>
 
 								<li><a href="https://plus.google.com/share?url={URL}" onclick=
 										"javascript:window.open(this.href,
-  										'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;"><img src="<?php bloginfo('template_url'); ?>/images/content/share/google_plus.png" width="16" height="16" alt="Google Plus Share" /></a></li>
+  										'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;"><img src="<?php bloginfo('template_url'); ?>/images/content/share/google2x.png" width="16" height="16" alt="Google Plus Share" /></a></li>
 
 								<li><a href="" onclick="window.open(
       									'http://www.reddit.com/submit?url=' + encodeURIComponent(window.location), 
       									'twitter-share-dialog'); 
-    									return false;"><img src="<?php bloginfo('template_url'); ?>/images/content/share/reddit.png" width="16" height="16" alt="Reddit Share" /></a></li>
+    									return false;"><img src="<?php bloginfo('template_url'); ?>/images/content/share/reddit2x.png" width="16" height="16" alt="Reddit Share" /></a></li>
 
-								<li><a href="mailto:?subject=<? echo $title; ?>&amp;body=<?php echo "http://" . $_SERVER['HTTP_HOST']  . $_SERVER['REQUEST_URI']; ?>"><img src="<?php bloginfo('template_url'); ?>/images/content/share/mail.png" width="16" height="16" alt="Mail Share" /></a></li>
+								<li><a href="mailto:?subject=<? echo $title; ?>&amp;body=<?php echo "http://" . $_SERVER['HTTP_HOST']  . $_SERVER['REQUEST_URI']; ?>"><img src="<?php bloginfo('template_url'); ?>/images/content/share/mail2x.png" width="16" height="16" alt="Mail Share" /></a></li>
 							</ul>
 						</div>
 
