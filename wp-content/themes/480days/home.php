9346567c5c082f9a72d0e8e7eabda598108eabe7
Michael Dziedzic <mdz@eggfoo.com>
2015-02-04 09:47:47 -0800

    copy change

--- wp-content/themes/480days/home.php
+++ wp-content/themes/480days/home.php
@@ -24,9 +24,9 @@
 							<p class="body-text-home">And in the interest of full disclosure, a small prop plane in Ethiopia from Lalibela to Addis Ababa as we had already done the trip in reverse by bus, it was near the end our trip, and we were running out steam (money, and, quite frankly, the will to live).</p>
 
 							<br/>
-							<p class="body-text-first-home"><sup>&dagger;</sup>Somehow making it despite untold malaria-carrying mosquitoes, a dangerously over-caffeinated monkey, and an  inebriated role-player with connections to the largest army of Dwarves in all of Eastern Europe.</p>
+							<p class="body-text-first-home"><sup>&dagger;</sup>Somehow making it despite untold malaria-carrying mosquitoes, a dangerously over-caffeinated monkey, and an  inebriated role-player with connections to the largest army of Dwarves in all of Eastern Europe (allegedly).</p>
 
-
+                        
 
 							<?php include (TEMPLATEPATH . '/share.php'); ?>
 
