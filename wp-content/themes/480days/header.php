2a6e1e8fa279f3e98d9d74b9478f3a6ad90af3b5
Michael Dziedzic <mdz@eggfoo.com>
2015-08-10 11:58:12 -0700

    cleaned up fonts, added our names to about section

--- wp-content/themes/480days/header.php
+++ wp-content/themes/480days/header.php
@@ -69,10 +69,8 @@
 	<!-- For non-Retina iPhone, iPod Touch, and Android -->
 	<link rel="<?php bloginfo('template_url'); ?>/images/touch/apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png" />	
 
-	<link href='http://fonts.googleapis.com/css?family=Libre+Baskerville' rel='stylesheet' type='text/css' />
 	<link href='http://fonts.googleapis.com/css?family=Sorts+Mill+Goudy' rel='stylesheet' type='text/css' />
 	<link href='http://fonts.googleapis.com/css?family=Sorts+Mill+Goudy:400italic' rel='stylesheet' type='text/css' />	  
-
 	<link rel="stylesheet" href="//f.fontdeck.com/s/css/3nQ4Zt9uZHi/fSeMr4rODjmORk4/DOMAIN_NAME/50825.css" type="text/css" />
 </head>
 
